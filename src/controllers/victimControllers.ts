d1d005986f58986940e4764629561ae6a2a7ea67
João Italo Deodato <166622223+Happjoi@users.noreply.github.com>
2025-05-15 16:44:46 -0300

    Update victimControllers.ts

--- src/controllers/victimControllers.ts
+++ src/controllers/victimControllers.ts
@@ -1,6 +1,6 @@
 import { Request, Response, NextFunction } from 'express';
 import mongoose from 'mongoose';
-import VictimModel, { IVictim } from '../models/victim'
+import VictimModel, { IVictim } from '../models/Victim'
 
 /**
  * Cria uma nova vítima
